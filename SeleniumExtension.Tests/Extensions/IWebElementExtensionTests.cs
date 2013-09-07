e54fc4b6b363798980563bcc702a34119a9a3fe5
Rick Casady <richard.casady@gmail.com>
2013-09-07 12:00:09 -0700

    Move Wait tests into the correct test classes
    created a IWebDriverExtensionTests.cs to hold the test for the extension
    class

--- SeleniumExtension.Tests/Extensions/IWebElementExtensionTests.cs
+++ SeleniumExtension.Tests/Extensions/IWebElementExtensionTests.cs
@@ -1,26 +1,27 @@
 ﻿using System.IO;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using TestWebPages.UIFramework.Pages;
 
 namespace SeleniumExtension.Tests
 {
     [TestFixture]
     public class IWebElementExtentionTests
     {
-        private IWebDriver driver = null;
+        private IWebDriver _driver = null;
         [SetUp]
         public void SetupTest()
         {
-            driver = IWebDriverFactory.GetBrowser(string.Format(@"file:///{0}../../../../TestWebPages/PageA.htm", Directory.GetCurrentDirectory()));
+            _driver = IWebDriverFactory.GetBrowser(string.Format(@"file:///{0}../../../../TestWebPages/PageA.htm", Directory.GetCurrentDirectory()));
         }
 
         [TearDown]
         public void TearDown()
         {
-            if (driver != null)
+            if (_driver != null)
             {
-                driver.Close();
-                driver.Quit();
+                _driver.Close();
+                _driver.Quit();
             }
         }
 
@@ -29,62 +30,62 @@ namespace SeleniumExtension.Tests
         [Test]
         public void TestClassNameExtention()
         {
-            Assert.AreEqual("myinput", driver.FindElement("text1").ClassName());
+            Assert.AreEqual("myinput", _driver.FindElement("text1").ClassName());
         }
 
         [Test]
         public void TestIdExtention()
         {
-            Assert.AreEqual("text1", driver.FindElement("text1").Id());
+            Assert.AreEqual("text1", _driver.FindElement("text1").Id());
         }
 
         [Test]
         public void TestNameExtention()
         {
-            Assert.AreEqual("text1", driver.FindElement("text1").Name());
+            Assert.AreEqual("text1", _driver.FindElement("text1").Name());
         }
 
         [Test]
         public void TestNextSiblingExtention()
         {
-            Assert.AreEqual(driver.FindElement("tdthree"), driver.FindElement("tdtwo").NextSibling());
+            Assert.AreEqual(_driver.FindElement("tdthree"), _driver.FindElement("tdtwo").NextSibling());
         }
 
         [Test]
         public void TestParentExtention()
         {
-            Assert.AreEqual(driver.FindElement("tdone"), driver.FindElement("text1").Parent());
+            Assert.AreEqual(_driver.FindElement("tdone"), _driver.FindElement("text1").Parent());
         }
 
         [Test]
         public void TestPreviousSiblingExtention()
         {
-            Assert.AreEqual(driver.FindElement("tdone"), driver.FindElement("tdtwo").PreviousSibling());
+            Assert.AreEqual(_driver.FindElement("tdone"), _driver.FindElement("tdtwo").PreviousSibling());
         }
 
         [Test]
         public void TestStyleExtention()
         {
-            string style = driver.FindElement("label1").Style().Replace(" ", "");
+            string style = _driver.FindElement("label1").Style().Replace(" ", "");
             Assert.AreEqual("background-color:red;", style);
         }
 
         [Test]
         public void TestTitleExtention()
         {
-            Assert.AreEqual("label one", driver.FindElement("label1").Title());
+            Assert.AreEqual("label one", _driver.FindElement("label1").Title());
         }
 
         [Test]
         public void TestTypeExtention()
         {
-            Assert.AreEqual("button", driver.FindElement("add1").Type());
+            Assert.AreEqual("button", _driver.FindElement("add1").Type());
         }
 
         [Test]
         public void TestValueExtention()
         {
-            Assert.AreEqual("add", driver.FindElement("add1").Value());
+            Assert.AreEqual("add", _driver.FindElement("add1").Value());
         }
 
         #endregion
@@ -92,19 +93,29 @@ namespace SeleniumExtension.Tests
         [TestCase("text1", "myinput")]
         public void TestGetAttributeClass(string id,  string expected)
         {
-            Assert.AreEqual(expected, driver.FindElement(id).GetAttribute(HtmlTagAttribute.Class));
+            Assert.AreEqual(expected, _driver.FindElement(id).GetAttribute(HtmlTagAttribute.Class));
         }
 
         [TestCase("label1", "label one")]
         public void TestGetAttributeTitle(string id, string expected)
         {
-            Assert.AreEqual(expected, driver.FindElement(id).GetAttribute(HtmlTagAttribute.Title));
+            Assert.AreEqual(expected, _driver.FindElement(id).GetAttribute(HtmlTagAttribute.Title));
         }
 
         [TestCase("add1", "add")]
         public void TestGetAttributeValue(string id,  string expected)
         {
-            Assert.AreEqual(expected, driver.FindElement(id).GetAttribute(HtmlTagAttribute.Value));
+            Assert.AreEqual(expected, _driver.FindElement(id).GetAttribute(HtmlTagAttribute.Value));
+        }
+
+        [TestCase(true, "red")]
+        [TestCase(false, "NeverGonnaGetNeverGonnaGet")]
+        public void TestWaitUntilExistsIWebElement(bool expected, string id)
+        {
+            string url = string.Format(@"file:///{0}../../../..{1}", Directory.GetCurrentDirectory(), AjaxyControlPage.Url);
+            _driver.Navigate().GoToUrl(url);
+            var body = _driver.FindElement(By.TagName("body"));
+            Assert.AreEqual(expected, body.WaitUntilExists(By.Id(id)));
         }
     }
 }
