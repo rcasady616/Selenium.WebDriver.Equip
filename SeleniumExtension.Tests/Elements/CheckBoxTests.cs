7229b929e15221532e9c082f57a925fc2f87ef54
Rick Casady <richard.casady@gmail.com>
2016-11-04 10:59:43 -0700

    Adding CheckBox tests

---
+++ SeleniumExtension.Tests/Elements/CheckBoxTests.cs
@@ -0,0 +1,61 @@
+﻿using NMock2;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumExtension.Elements;
+
+namespace SeleniumExtension.Tests.Elements
+{
+    class CheckBoxTests
+    {
+    }
+
+    [TestFixture]
+    public class HeadLessCheckBoxTests
+    {
+        private Mockery mocks;
+        private IWebElement iWebElement;
+        private CheckBox checkBox;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mocks = new Mockery();
+            iWebElement = mocks.NewMock<IWebElement>();
+            checkBox = new CheckBox(iWebElement);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void CheckBoxSelected(bool selected)
+        {
+            Stub.On(iWebElement).GetProperty("Selected").Will(Return.Value(selected));
+            Assert.AreEqual(selected, checkBox.Selected);
+        }
+
+        [Test]
+        public void CheckBoxUnChecked()
+        {
+            Expect.Once.On(iWebElement).GetProperty("Selected").Will(Return.Value(true));
+            Assume.That(true == checkBox.Selected);
+            Expect.Once.On(iWebElement).GetProperty("Selected").Will(Return.Value(true));
+            Expect.Once.On(iWebElement).Method("Click");
+            checkBox.UnCheck();
+            Expect.Once.On(iWebElement).GetProperty("Selected").Will(Return.Value(false));
+            Assert.AreEqual(false, checkBox.Selected);
+            mocks.VerifyAllExpectationsHaveBeenMet();
+        }
+
+        [Test]
+        public void CheckBoxChecked()
+        {
+            Expect.Once.On(iWebElement).GetProperty("Selected").Will(Return.Value(false));
+            Assume.That(false == checkBox.Selected);
+            Expect.Once.On(iWebElement).GetProperty("Selected").Will(Return.Value(false));
+            Expect.Once.On(iWebElement).Method("Click");
+            checkBox.Check();
+            Expect.Once.On(iWebElement).GetProperty("Selected").Will(Return.Value(true));
+            Assert.AreEqual(true, checkBox.Selected);
+            mocks.VerifyAllExpectationsHaveBeenMet();
+        }
+    }
+}
