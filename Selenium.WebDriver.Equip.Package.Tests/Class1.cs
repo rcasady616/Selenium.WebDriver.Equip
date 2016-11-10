ab22a298ea6d042a239df8905b0371cafdddd01f
Rick Casady <richard.casady@gmail.com>
2016-11-09 16:34:18 -0800

    Building Nuget package at the end of successfull build

---
+++ Selenium.WebDriver.Equip.Package.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.WebDriver.Equip.Package.Tests
+{
+    public class Class1
+    {
+    }
+}
