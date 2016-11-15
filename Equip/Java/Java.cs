fb646c141000eed7a89425bbfbd6014b2e7e7364
Rick Casady <richard.casady@gmail.com>
2016-11-14 16:50:09 -0800

    Missing file from last checkin

--- Equip/Java/Java.cs
+++ Equip/Java/Java.cs
@@ -4,6 +4,7 @@ namespace Equip.Java
 {
     public class Java
     {
+        public static string GetJreVersion()
         {
             string jreRegistryPath = "SOFTWARE\\JavaSoft\\Java Runtime Environment";
             return Registry.LocalMachine.GetRegistryKey(jreRegistryPath).GetKeyValue("CurrentVersion");
