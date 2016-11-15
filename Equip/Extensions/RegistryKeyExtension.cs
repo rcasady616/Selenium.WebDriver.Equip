4aab55bd5251f5d34b0babb895afafb5e91a42b6
Rick Casady <richard.casady@gmail.com>
2016-11-14 16:32:30 -0800

    created a way to see if java is installed and in the path and return the version

---
+++ Equip/Extensions/RegistryKeyExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.Win32
+{
+    public static  class RegistryKeyExtension
+    {
+        public static string GetKeyValue(this RegistryKey registryKey, string key)
+        {
+            if (registryKey == null)
+                return null;
+            return registryKey.GetValue(key).ToString();
+        }
+
+        public static RegistryKey GetRegistryKey(this RegistryKey registryKey, string key)
+        {
+            return registryKey.OpenSubKey(key);
+        }
+    }
+}
---
+++ Equip/Java/Java.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Win32;
+
+namespace Equip.Java
+{
+    public class Java
+    {
+        {
+            string jreRegistryPath = "SOFTWARE\\JavaSoft\\Java Runtime Environment";
+            return Registry.LocalMachine.GetRegistryKey(jreRegistryPath).GetKeyValue("CurrentVersion");
+        }
+    }
+}
