5a3be346b682dfe896c2f664c134b75ca54f82dc
Rick Casady <richard.casady@gmail.com>
2021-07-24 15:22:06 -0700

    Update WebDriverExtensionTests.cs

--- Selenium.WebDriver.Equip.Tests/Extensions/WebDriverExtensionTests.cs
+++ Selenium.WebDriver.Equip.Tests/Extensions/WebDriverExtensionTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System.Collections.Generic;
 using System.IO;
